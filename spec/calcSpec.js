9772033ac91bc08e2a13779d6be91631e1c3c38f
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-02-14 12:33:11 +0000

    03-red_green_refactor

---
+++ spec/calcSpec.js
@@ -0,0 +1,7 @@
+describe("Calculator", function () {
+    describe("Addition tests", function () {
+        it("should return 42", function () {
+            expect(addition(20, 20)).toBe(42)
+        });
+    });
+});
